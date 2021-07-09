033877b837d7887d152f30aa732b37a07fbfb512
ChanceMaston <chance.maston@cradlepoint.com>
2021-07-09 00:27:40 -0600

    Defined Models 'Movie' and 'Actor'

--- models.py
+++ models.py
@@ -1,1 +1,166 @@
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
 
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+
+db = SQLAlchemy()
+
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+'''
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+    # add one demo row which is helping in POSTMAN test
+    drink = Drink(
+        title='water',
+        recipe='[{"name": "water", "color": "blue", "parts": 1}]'
+    )
+    drink.insert()
+
+# ROUTES
+'''
+Movie
+a persistent movie entity, extends the base SQLAlchemy Model
+'''
+class Movie(db.Model):
+    # Autoincrementing, unique primary key
+    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+    # String Title
+    title = Column(String(80), unique=True)
+    # The Release date of the movie
+    # the required datatype is [{'day': string, 'month':string, 'year':string}]
+    release_date = Column(String(180), nullable=False)
+
+    '''
+    long()
+        long form representation of the Movie model
+    '''
+    def long(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': json.loads(self.release_date)
+        }
+
+    '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+        EXAMPLE
+            movie = Movie(title=req_title, release_date=req_release_date)
+            movie.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie(title=req_title, release_date=req_release_date)
+            movie.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            movie = Movie.query.filter(Movie.id == id).one_or_none()
+            movie.title = 'Watchmen'
+            movie.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.short())
+
+
+'''
+Actor
+a persistent actor entity, extends the base SQLAlchemy Model
+'''
+class Actor(db.Model):
+    # Autoincrementing, unique primary key
+    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+    # String Name
+    name = Column(String(80), unique=True)
+    # Int age
+    age = Column(Integer(), nullable=False)
+    # String Gender
+    gender = Column(String(80), unique=False, nullable=False)
+
+    '''
+    long()
+        long form representation of the Movie model
+    '''
+    def long(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+    '''
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+        EXAMPLE
+            actor = Actor(name=req_name, age=req_age, gender=req_gender)
+            actor.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            actor = Actor(name=req_name, age=req_age, gender=req_gender)
+            actor.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            actor = Actor.query.filter(Actor.id == id).one_or_none()
+            actor.name = 'JK Simmons'
+            actor.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.short())
